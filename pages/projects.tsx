59a962b705b08f7eed39ea6993c8f398c4fb6fd9
bryanmoon1991 <bryanmoon1991@gmail.com>
2021-02-17 22:43:20 -0500

    added more information to portfolio page, need to organize data better

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -39,8 +39,16 @@ const Portfolio: React.FunctionComponent = () => (
 							<strong>Problems I Encountered</strong>
 						</ListItem>
 						<ListItem>
-							<Text>
+							<Text maxWidth="sm">
 								&bull; Tokens provided to users expire every 30 minutes
+							</Text>
+							<Text maxWidth="sm">
+								&bull; Results from API calls that exceed a certain number are
+								sent with an endpoint for the 'next page' of results
+							</Text>
+							<Text maxWidth="sm">
+								&bull; The 'artist' field for compilation albums are labeled as
+								'Various Artists' along with a corresponding id
 							</Text>
 						</ListItem>
 						<ListItem as="h4">
@@ -48,10 +56,23 @@ const Portfolio: React.FunctionComponent = () => (
 							<strong>My Solutions</strong>
 						</ListItem>
 						<ListItem>
-							<Text>
-								&bull; I built a method that was called in the catch blocks of
+							<Text maxWidth="sm">
+								&bull; I built a function that was called in the catch blocks of
 								my API requests so that if a call had failed, I could refresh
 								the token without disturbing the user's experience.
+							</Text>
+							<Text maxWidth="sm">
+								&bull; I set state with the initial results of the API call and
+								as the first few results would render, a function that checks if
+								there were more results would run asynchronously and continue to
+								update the piece of state that held its preceding data.
+							</Text>
+							<Text maxWidth="sm">
+								&bull; Compilations were one of my main edgecases I had to test
+								against. For these items, I had to scan each track and extract
+								the id's of each artist that appeared on the album. Using these
+								ids I was able to create a component that accurately displayed
+								the artists that appear on the item.
 							</Text>
 						</ListItem>
 					</List>
